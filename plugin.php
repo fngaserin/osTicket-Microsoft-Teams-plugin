ec8cdfd45f486d3fbef55313cfc455d302f48077
Felix Ngaserin <felix.ngaserin@mandiri-investasi.co.id>
2020-04-21 23:28:41 +0700

    Bump up the version to 1.0

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 
 return array(
     'id'          => 'osticket:teams',
-    'version'     => '0.1',
-    'name'        => 'Teams notifier',
-    'author'      => 'Ivan Pavlović',
+    'version'     => '1.0',
+    'name'        => 'Microsoft Teams Notifier',
+    'author'      => 'Felix Ngaserin',
     'description' => 'Notify Teams on new ticket.',
-    'url'         => 'https://github.com/ipavlovi/osTicket-Microsoft-Teams-plugin',
+    'url'         => 'https://github.com/fngaserin/osTicket-Microsoft-Teams-plugin',
     'plugin'      => 'teams.php:TeamsPlugin',
 );
